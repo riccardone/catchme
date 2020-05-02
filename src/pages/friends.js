7b23447c389572dbc411130d72b34a13e2574684
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2020-05-02 16:04:41 +0100

    start

---
+++ src/pages/friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Friends extends React.Component {
+
+    render() {
+        return <div className="App">
+            <header className="App-header">                      
+                <p>friends</p>
+            </header>
+        </div>
+    }
+}
